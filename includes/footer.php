5ee698f7ef2d4ebd0678e03f64fc9a877c97c61a
CHHUB RACHHEN <165193974+chhubrachhen276@users.noreply.github.com>
2026-06-19 22:25:26 +0700

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+    <footer class="py-5 bg-primary">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; E-FLEX RUN <?php echo date('Y');?></p>
+      </div>
+      <!-- /.container -->
+    </footer>
